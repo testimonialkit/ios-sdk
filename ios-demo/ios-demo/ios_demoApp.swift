5a430f2d1776fe05ee4b759eee5f8e007b73c675
Charlyk <eduard.albu@gmail.com>
2025-08-07 20:29:49 +0300

    Added demo app to git

---
+++ ios-demo/ios-demo/ios_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_demoApp.swift
+//  ios-demo
+//
+//  Created by Eduard Albu on 07.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
