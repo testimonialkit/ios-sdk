6cefa2f5b263afddab4a13dffbc2e3439c95ba96
Eduard Albu <eduard.albu@gmail.com>
2025-08-13 11:04:07 +0300

    Implemented prompt UI and most of the logic.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
   name: "TestimonialKit",
   platforms: [
-    .iOS("15.0") // ✅ specify the minimum iOS version
+    .iOS("15.0"),
+    .macOS("12.0")
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -16,9 +17,9 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/Charlyk/swift-theme-kit.git", from: "1.0.4"),
+    .package(url: "https://github.com/Charlyk/swift-theme-kit.git", branch: "fixAssets"),
     .package(url: "https://github.com/hmlongco/Factory.git", from: "2.5.3"),
-    .package(url: "https://github.com/simibac/ConfettiSwiftUI.git", from: "2.0.3")
+    .package(url: "https://github.com/simibac/ConfettiSwiftUI.git", from: "2.0.3"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
